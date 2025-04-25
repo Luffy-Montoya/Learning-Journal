5996eb2e7bcdc19f7c8f24f338d4c70def283cf1
Luffy-Montoya <80363512+Luffy-Montoya@users.noreply.github.com>
2025-04-24 23:13:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ let viewMore = document.getElementById("view-more-link")
 
 viewMore.addEventListener("click", viewMoreBlogs)
 
+let viewMoreOn = false
+
 function viewMoreBlogs() {
     document.getElementById("blog-4").classList.toggle("hidden")
     document.getElementById("blog-5").classList.toggle("hidden")
     document.getElementById("blog-6").classList.toggle("hidden")
     console.log("clicked!")
+
+    if (viewMoreOn === false) {
+        viewMore.innerHTML = "View Less"
+        viewMoreOn = true
+    } else {
+        viewMore.innerHTML = "View More"
+        viewMoreOn = false
+    }
 }
